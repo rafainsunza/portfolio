f857cd51487b778728a1938083d47fb59a60e030
rafainsunza <rafainsunza@outlook.com>
2025-10-31 13:48:59 +0100

    Initial commit: React setup

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <nav>
+          <img src="" alt="" />
+          <a href="home">Rafael</a>
+          <a href="projects">See what I've built</a>
+          <a href="cv">Peek at my CV</a>
+          <a href="contact">Get in touch</a>
+        </nav>
+
+        <div className="toggle-container"></div>
+      </header>
+
+      <main></main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
