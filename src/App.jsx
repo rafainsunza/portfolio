47678d68b29cfcff88fa6894c6f7926ceef31349
rafainsunza <rafainsunza@outlook.com>
2025-11-03 11:15:24 +0100

    Create website structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,51 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
+  const toggleTheme = () => {
+    const htmlElement = document.documentElement;
+    const isDarkMode = htmlElement.hasAttribute("data-theme");
+
+    isDarkMode ? htmlElement.removeAttribute("data-theme", "dark") : htmlElement.setAttribute("data-theme", "dark");
+  };
+
   return (
     <>
       <header>
         <nav>
-          <img src="" alt="" />
-          <a href="home">Rafael</a>
-          <a href="projects">See what I've built</a>
-          <a href="cv">Peek at my CV</a>
-          <a href="contact">Get in touch</a>
+          <a href="#hero">home</a>
+          <a href="#projects" className="nav-link">
+            See what I've built
+          </a>
+          <a href="#cv" className="nav-link">
+            Peek at my CV
+          </a>
+          <a href="#about" className="nav-link">
+            About me
+          </a>
+          <a href="#contact" className="nav-link">
+            Get in touch
+          </a>
         </nav>
 
-        <div className="toggle-container"></div>
+        <div className="toggle-container">
+          <button className="toggle-button" onClick={() => toggleTheme()}>
+            lig dar
+          </button>
+          <button className="toggle-button">LAN</button>
+        </div>
       </header>
 
-      <main></main>
+      <main>
+        <section id="hero">HI IM RAFA</section>
+
+        <section id="projects">PROJECTS</section>
+
+        <section id="cv">CV</section>
+
+        <section id="about">ABOUT</section>
+
+        <section id="contact">CONTACT</section>
+      </main>
       <footer></footer>
     </>
   );
