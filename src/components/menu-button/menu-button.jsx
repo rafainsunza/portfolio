8654cfa12b1e76a9d1ea9a3fb8a3f5b5a2fe2eaf
rafainsunza <rafainsunza@outlook.com>
2025-11-06 08:54:36 +0100

    Create menu button for mobile

--- src/components/menu-button/menu-button.jsx
+++ src/components/menu-button/menu-button.jsx
@@ -2,17 +2,20 @@ import { useState } from "react";
 import "./menu-button.scss";
 
 function MenuButton() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isToggled, setIsToggled] = useState(false);
 
   const handleMenuButtonClick = () => {
     const navbar = document.querySelector(".navbar");
-
     navbar.classList.toggle("open");
+
+    isToggled ? setIsToggled(false) : setIsToggled(true);
   };
 
   return (
-    <button className="menu-button" onClick={handleMenuButtonClick}>
-      AS
+    <button className={!isToggled ? "menu-button" : "menu-button toggled"} onClick={handleMenuButtonClick}>
+      <span className="menu-button__bar"></span>
+      <span className="menu-button__bar"></span>
+      <span className="menu-button__bar"></span>
     </button>
   );
 }
