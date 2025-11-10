1a44f43585d93d09d9e9dfe6783332fe392dd448
rafainsunza <rafainsunza@outlook.com>
2025-11-10 19:27:05 +0100

    Slight restyle in hero and remove imgae

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,24 +1,17 @@
 import "./hero.scss";
-import portrait from "../../assets/images/cv-photo.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import SocialMediaBox from "../social-media-box/social-media-box";
 
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="hero__title-wrapper">
-        <h1 className="hero__title">
-          Hi,
-          <br /> I'm Rafael
-        </h1>
-        <h2 className="hero__subtitle">I love turning designs into smooth, responsive websites.</h2>
-      </div>
+      <h1 className="hero__title">
+        Hi,
+        <br /> I'm Rafael
+      </h1>
+      <h2 className="hero__subtitle">I love turning designs into smooth, responsive websites.</h2>
 
       <SocialMediaBox />
-
-      <div className="hero__image">
-        <img src={portrait} alt="" />
-      </div>
     </div>
   );
 };
