176269ef7bf79ddfaa675914d92e42f2e6361877
rafainsunza <rafainsunza@outlook.com>
2025-11-14 10:36:19 +0100

    Dynamically create project cards

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,23 +1,40 @@
 import "./projects.scss";
+import projectData from "../../data/static/projects.json";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useTranslations } from "../../context/language-context";
+import WebComponentsIcon from "../web-components-icon/web-components-icon";
+import { faGithub, faJs, faReact } from "@fortawesome/free-brands-svg-icons";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
 
 const Projects = () => {
   const { translation } = useTranslations();
-  const projects = ["onderde10", "eredivisie", "ecommercepage", "eitalexander", "infobycountry"];
 
   return (
     <div className="projects">
       <h1 className="section__title">{translation("projects.title")}</h1>
 
       <div className="projects__cards">
-        {projects.map((project) => (
-          <div className="projects__card">
-            <div className="projects__card-title">{project}</div>
-            <div className="projects__card-preview"></div>
-            <div className="projects__card-description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi natus adipisci facilis eaque tempore sed!
+        {projectData.map((project) => (
+          <div className="projects__card" key={project.name}>
+            <div className="projects__card-title">{project.name}</div>
+            <a href={project.href} rel="noopener noreferrer" target="_blank" className="projects__card-preview">
+              <img src={project.screenshot} alt="" />
+            </a>
+            <div className="projects__card-description">{translation(`projects.cards.${project.id}.description`)}</div>
+            <div className="projects__card-tech" title={project.tech.replace(/\b\w/g, (char) => char.toUpperCase())}>
+              {project.tech === "react" && <FontAwesomeIcon icon={faReact} />}
+              {project.tech === "web components" && <WebComponentsIcon />}
+              <div className="projects__card-tech__label">{translation("projects.tech-used")}</div>
             </div>
+            <a
+              href={project.gitHubHref}
+              rel="noopener noreferrer"
+              target="_blank"
+              className="projects__card-github-link"
+            >
+              <FontAwesomeIcon icon={faGithub} />
+              <div className="projects__card-github-link__label">{translation("projects.view-repo")}</div>
+            </a>
           </div>
         ))}
       </div>
---
+++ src/components/slider-bullets/slider-bullets.jsx
@@ -0,0 +1,9 @@
+import "./slider-bullets.scss";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const SliderBullets = () => {
+  return <div className="slider-bullets"></div>;
+};
+
+export default SliderBullets;
