df8c6bc850ae3885f332467064c9168e0fc2f162
rafainsunza <rafainsunza@outlook.com>
2025-11-18 16:34:44 +0100

    Changes to cards layout

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -3,8 +3,7 @@ import projectData from "../../data/static/projects.json";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useTranslations } from "../../context/language-context";
 import WebComponentsIcon from "../web-components-icon/web-components-icon";
-import { faGithub, faJs, faReact } from "@fortawesome/free-brands-svg-icons";
-import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faReact } from "@fortawesome/free-brands-svg-icons";
 
 const Projects = () => {
   const { translation } = useTranslations();
@@ -15,12 +14,19 @@ const Projects = () => {
 
       <div className="projects__cards">
         {projectData.map((project) => (
-          <div className="projects__card" key={project.name}>
-            <div className="projects__card-title">{project.name}</div>
-            <a href={project.href} rel="noopener noreferrer" target="_blank" className="projects__card-preview">
+          <a
+            href={project.href}
+            rel="noopener noreferrer"
+            target="_blank"
+            className="projects__card"
+            key={project.name}
+          >
+            <div className="projects__card-preview">
               <img src={project.screenshot} alt="" />
-            </a>
-            <div className="projects__card-description">{translation(`projects.cards.${project.id}.description`)}</div>
+            </div>
+            <h3 className="projects__card-title">{project.name}</h3>
+
+            <p className="projects__card-description">{translation(`projects.cards.${project.id}.description`)}</p>
             <div className="projects__card-tech" title={project.tech.replace(/\b\w/g, (char) => char.toUpperCase())}>
               {project.tech === "react" && <FontAwesomeIcon icon={faReact} />}
               {project.tech === "web components" && <WebComponentsIcon />}
@@ -35,7 +41,7 @@ const Projects = () => {
               <FontAwesomeIcon icon={faGithub} />
               <div className="projects__card-github-link__label">{translation("projects.view-repo")}</div>
             </a>
-          </div>
+          </a>
         ))}
       </div>
     </div>
