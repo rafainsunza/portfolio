86363f6639943861b1eb5363f90263a03f8950ea
rafainsunza <rafainsunza@outlook.com>
2025-11-19 16:14:59 +0100

    Style slider bullets

--- src/components/slider-bullets/slider-bullets.jsx
+++ src/components/slider-bullets/slider-bullets.jsx
@@ -4,7 +4,12 @@ const SliderBullets = ({ sliderBulletCount, currentPage }) => {
   return (
     <div className="slider-bullets">
       {Array.from({ length: sliderBulletCount }).map((_, i) => (
-        <div key={i} className={`slider-bullets__circle ${currentPage === i ? "active" : ""}`}></div>
+        <div
+          key={i}
+          className={`slider-bullets__circle ${currentPage === i ? "active" : ""} ${
+            i === currentPage - 1 || i === currentPage + 1 ? "neighbor" : ""
+          } `}
+        ></div>
       ))}
     </div>
   );
