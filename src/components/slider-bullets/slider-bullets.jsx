74a7e53ea0c0ec8a719a1a58cc2ced4859d468e9
rafainsunza <rafainsunza@outlook.com>
2025-11-19 16:23:45 +0100

    Replace bullet div with icon

--- src/components/slider-bullets/slider-bullets.jsx
+++ src/components/slider-bullets/slider-bullets.jsx
@@ -1,15 +1,18 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./slider-bullets.scss";
+import { faDiamond } from "@fortawesome/free-solid-svg-icons";
 
 const SliderBullets = ({ sliderBulletCount, currentPage }) => {
   return (
     <div className="slider-bullets">
       {Array.from({ length: sliderBulletCount }).map((_, i) => (
-        <div
+        <FontAwesomeIcon
+          icon={faDiamond}
           key={i}
           className={`slider-bullets__circle ${currentPage === i ? "active" : ""} ${
             i === currentPage - 1 || i === currentPage + 1 ? "neighbor" : ""
           } `}
-        ></div>
+        ></FontAwesomeIcon>
       ))}
     </div>
   );
