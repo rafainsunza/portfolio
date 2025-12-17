ebbd68864eb3e96b873f4644b130e772b8b80860
rafainsunza <rafainsunza@outlook.com>
2025-12-17 21:16:19 +0100

    Build and style about cards

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -2,21 +2,34 @@ import "./about.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useTranslations } from "../../context/language-context";
 import {
+  faBaby,
   faBellConcierge,
+  faBoltLightning,
   faChalkboardTeacher,
   faChampagneGlasses,
   faCode,
+  faComputerMouse,
+  faDisplay,
+  faDumbbell,
   faFolderTree,
+  faFutbol,
+  faGlobe,
   faLaptopCode,
   faMartiniGlassCitrus,
+  faMugHot,
   faUmbrellaBeach,
   faUserTie,
   faUtensils,
 } from "@fortawesome/free-solid-svg-icons";
+import { faGitAlt, faJs, faReact, faSass } from "@fortawesome/free-brands-svg-icons";
+
+import CustomIcon from "../custom-icon/custom-icon";
 import { useEffect, useState } from "react";
+import { faClock } from "@fortawesome/free-regular-svg-icons";
 
 const About = () => {
   const { translation } = useTranslations();
+
   const timeline = translation("about.timeline");
   const half = Math.ceil(timeline.length / 2);
   const [items, setItems] = useState({
@@ -35,6 +48,20 @@ const About = () => {
     laptop: faLaptopCode,
     folder: faFolderTree,
     cheers: faChampagneGlasses,
+    baby: faBaby,
+    coffee: faMugHot,
+    training: faDumbbell,
+    football: faFutbol,
+    js: faJs,
+    react: faReact,
+    sass: faSass,
+    editor: faLaptopCode,
+    git: faGitAlt,
+    vite: faBoltLightning,
+    browser: faGlobe,
+    screen: faDisplay,
+    mouse: faComputerMouse,
+    clock: faClock,
   };
 
   useEffect(() => {
@@ -66,6 +93,40 @@ const About = () => {
 
   return (
     <div className="about">
+      <div className="about__cards">
+        {translation("about.cards").map((card, index) => (
+          <div className="about__card" key={index}>
+            <h4 className="about__card__title">
+              {card.title.split(",").map((part, i) => (
+                <span key={part}>
+                  {part.trim()}
+                  {i === 0 && <br />}
+                </span>
+              ))}
+            </h4>
+            {card.content.map((content) => (
+              <div className="about__card__content" key={content.id}>
+                <div className="about__card__icon">
+                  {content.id === "webcomponents" ? (
+                    <CustomIcon size={23} icon={"webcomponents"} />
+                  ) : content.id === "bjj" ? (
+                    <CustomIcon size={25} icon={"bjj"} />
+                  ) : (
+                    <FontAwesomeIcon icon={icons[content.icon]} />
+                  )}
+                </div>
+                <div className="about__card__label__value__wrapper">
+                  <div className="about__card__label">{content.label}</div>
+                  {content.value && <div className="about__card__value">{content.value}</div>}
+                </div>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+
+      <h4 className="about__timeline__title">From then to now</h4>
+
       <div className="about__timeline">
         <div className="about__timeline__line">
           <div className="about__timeline__line__start"></div>
